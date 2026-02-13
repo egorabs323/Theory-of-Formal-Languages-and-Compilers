43b2dd8e5cf15a20b16c6c5210be8181e1e03c40
egoroabs323 <egoroleinik19@gmail.com>
2026-02-13 23:09:00 +0700

    горячие клавиши

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace YourNamespace
@@ -639,6 +640,85 @@ namespace YourNamespace
                             "Выполнил -> Студент гр.АП-326 Олейник Е.В.\n" +
                             "Проверил -> Ассистент АСУ Антонянц Е.Н.",
                             LocalizationManager.GetString("AboutTitle"));
+        }
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyboardDevice.Modifiers == ModifierKeys.Control)
+            {
+                switch (e.Key)
+                {
+                    case Key.N:
+                        Create_Click(null, null);
+                        e.Handled = true;
+                        break;
+                    case Key.O:
+                        Open_Click(null, null);
+                        e.Handled = true;
+                        break;
+                    case Key.S:
+                        if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                        {
+                            SaveAs_Click(null, null);
+                        }
+                        else
+                        {
+                            Save_Click(null, null);
+                        }
+                        e.Handled = true;
+                        break;
+                    case Key.W:
+                        Close_Click(null, null);
+                        e.Handled = true;
+                        break;
+                    case Key.Z:
+                        Undo_Click(null, null);
+                        e.Handled = true;
+                        break;
+                    case Key.Y:
+                        Redo_Click(null, null);
+                        e.Handled = true;
+                        break;
+                    case Key.X:
+                        Cut_Click(null, null);
+                        e.Handled = true;
+                        break;
+                    case Key.C:
+                        Copy_Click(null, null);
+                        e.Handled = true;
+                        break;
+                    case Key.V:
+                        Paste_Click(null, null);
+                        e.Handled = true;
+                        break;
+                    case Key.A:
+                        SelectAll_Click(null, null);
+                        e.Handled = true;
+                        break;
+                }
+            }
+            else if (e.KeyboardDevice.Modifiers == ModifierKeys.Alt && e.Key == Key.F4)
+            {
+                Exit_Click(null, null);
+                e.Handled = true;
+            }
+            else if (e.KeyboardDevice.Modifiers == ModifierKeys.Control)
+            {
+                if (e.Key == Key.Add || e.Key == Key.OemPlus)
+                {
+                    IncreaseFontSize_Click(null, null);
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.Subtract || e.Key == Key.OemMinus)
+                {
+                    DecreaseFontSize_Click(null, null);
+                    e.Handled = true;
+                }
+            }
+            else if (e.Key == Key.Delete)
+            {
+                Delete_Click(null, null);
+                e.Handled = true;
+            }
         }
     }
 }
