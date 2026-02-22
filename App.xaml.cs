2bddf1cf533b0641ed17cb8b975402976c7eee76
egoroabs323 <egoroleinik19@gmail.com>
2026-02-22 15:53:42 +0700

    вроде все сделано

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace laba1
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Ловим критические ошибки до запуска UI
+            AppDomain.CurrentDomain.UnhandledException += (s, args) =>
+            {
+                Exception ex = args.ExceptionObject as Exception;
+                MessageBox.Show($"КРИТИЧЕСКАЯ ОШИБКА:\n{ex?.Message}\n\n{ex?.StackTrace}",
+                                "Ошибка запуска", MessageBoxButton.OK, MessageBoxImage.Error);
+            };
+
+            // Ловим ошибки в потоке интерфейса
+            DispatcherUnhandledException += (s, args) =>
+            {
+                MessageBox.Show($"ОШИБКА:\n{args.Exception.Message}",
+                                "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                args.Handled = true;
+            };
+        }
     }
 }
